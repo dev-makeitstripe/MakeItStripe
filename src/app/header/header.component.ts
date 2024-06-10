e68adb0aace5fc7bfe4122a6ac2bc674ca7341fc
dev-makeitstripe <admin@makeitstripe.com>
2024-06-10 13:27:03 -0400

    updates for supporting mobile devices.

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,12 +1,23 @@
+import { NgClass } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [NgClass],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+  showMobileNav: boolean = false
 
+  showNav($event: any) {
+    $event.preventDefault();
+    this.closeMobileNav();
+  }
+
+  closeMobileNav()
+  {
+    this.showMobileNav = !this.showMobileNav;
+  }
 }
